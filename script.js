05e86d6d130fc37a6b346553559f0fc180c2a4c9
Codelingo Tutorials <codelingotutorial@gmail.com>
2024-11-17 10:45:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.querySelector("nav").addEventListener("click", (e) => {
+  if (e.target.tagName == "BUTTON") {
+    document.querySelector("nav .active").classList.remove("active");
+    e.target.classList.add("active");
+  }
+});
